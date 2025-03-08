2cc6976e10e24adea42a01e555fe2ab466f40809
gabrielms-castro <gabrielms.castro@gmail.com>
2025-03-08 14:48:16 -0300

    feat - HTMLNode clas. It represent a node in a HTML Tree

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag              # A string representing the HTML tag name (e.g. "p", "a", "h1", etc.)
+        self.value = value          # A string representing the value of the HTML tag (e.g. the text inside a paragraph)
+        self.children = children    # A list of HTMLNode objects representing the children of this node
+        self.props = props          # A dictionary of key-value pairs representing the attributes of the HTML tag. For example, a link (<a> tag) might have {"href": "https://www.google.com"}
+    
+    def to_html(self):
+        raise NotImplementedError # child classes will override this
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self):
+        return f"HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, prop: {self.props})"
