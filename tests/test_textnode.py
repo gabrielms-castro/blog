3f6aa90bb0844bfbd04111558eaea622b3799c4a
gabrielms-castro <gabrielms.castro@gmail.com>
2025-03-16 12:37:24 -0300

    feat - add text_to_textnodes

--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.inline_markdown import text_to_textnodes
 from src.textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
@@ -60,6 +61,26 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
+
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
